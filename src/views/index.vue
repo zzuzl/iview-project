345453297341dd0a410fa85d4af5d5304918c470
zhanglei53 <zhanglei53@jd.com>
2018-11-22 15:14:51 +0800

    Initial commit

---
+++ src/views/index.vue
@@ -0,0 +1,66 @@
+<style scoped lang="less">
+</style>
+<template>
+    <Row>
+        <Col span="24">
+            <Button type="primary" @click="handleStart">添加</Button>
+            <Table :columns="columns1" :data="data1"></Table>
+        </Col>
+    </Row>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                columns1: [
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                data1: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ]
+            }
+        },
+        methods: {
+            handleStart () {
+                this.$Modal.info({
+                    title: 'Bravo',
+                    content: 'Now, enjoy the convenience of iView.'
+                });
+            }
+        }
+    }
+</script>
